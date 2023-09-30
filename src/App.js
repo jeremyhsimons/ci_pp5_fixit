a7ce704c33680a7b9b3bb2c9a11a16056ca23e60
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-30 15:22:14 +0000

    Install reactrouterdom and set up switches

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import styles from './App.module.css';
 import NavigationBar from './components/NavigationBar';
+import { Container } from 'react-bootstrap';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
     <div className={styles.App}>
-      <header className="App-header">
-        <NavigationBar />
-      </header>
+      <NavigationBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
