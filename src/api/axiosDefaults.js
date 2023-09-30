ec6d1cc5d3dd06904c33f98722011d70e46072f3
jeremyhsimons <jeremysimons15@gmail.com>
2023-09-30 16:04:20 +0000

    set up axios defaults ready for api calls.

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://fixit-drf-api-b3b58b2bc39c.herokuapp.com/"
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
