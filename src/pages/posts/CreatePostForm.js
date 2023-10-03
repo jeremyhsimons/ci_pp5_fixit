6744b4a5c0800884af8d5425803125ca7dcf589b
jeremyhsimons <jeremysimons15@gmail.com>
2023-10-03 19:02:02 +0000

    add basic post form fields.

--- src/pages/posts/CreatePostForm.js
+++ src/pages/posts/CreatePostForm.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Form } from 'react-bootstrap';
 
 const CreatePostForm = () => {
+  const [errors, setErrors] = useState();
   return (
-    <div><h2>This is the form to create a post</h2></div>
-  )
+    <Form>
+      <Container>
+        <Form.Group controlId="title">
+          <Form.Label>Title</Form.Label>
+          <Form.Control type="text" name="title" className={styles.Input} value={title} onChange={handleChange}/>
+        </Form.Group>
+        <Form.Group controlId="title">
+          <Form.Label>Content</Form.Label>
+          <Form.Control as="textarea" name="content" className={styles.Input} value={content} onChange={handleChange}/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Upload an image</Form.Label>
+          <Form.File id="image-upload" accept="image/*" onChange={handleChangeImage} ref={inputImage}></Form.File>
+        </Form.Group>
+      </Container>
+    </Form>
+  );
 }
 
 export default CreatePostForm
