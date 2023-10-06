993be8d4a5ba20daf4d9fd717293134e7f535615
jeremyhsimons <jeremysimons15@gmail.com>
2023-10-06 07:51:31 +0000

    Add logic to handle upvoting posts.

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useCurrentUser } from '../../contexts/CurrentUserContext'
-import { Card, Media } from 'react-bootstrap';
+import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Post = (props) => {
@@ -47,6 +47,32 @@ const Post = (props) => {
           {title && <Card.Title className="text-center">{title}</Card.Title>}
           {content && <Card.Text className="text-center">{content}</Card.Text>}
           <div>
+            {is_owner ? (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>You can't upvote your own post!</Tooltip>}
+              >
+                <p>cant upvote your post!</p>
+              </OverlayTrigger>
+            ) : upvote_id ? (
+              <span onClick={() => {}}>
+                <p>un-upvote!</p>
+                {/* Handles un-upvoting the post */}
+              </span>
+            ) : currentUser ? (
+              <span onClick={() => {}}>
+                <p>upvote!</p>
+                {/* Handles upvoting the post */}
+              </span>
+            ) : (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Log in to upvote posts!</Tooltip>}
+              >
+                <p>Log in to upvote!</p>
+                {/* handles users not logged in, and can't upvote */}
+              </OverlayTrigger>
+            )}
             {upvotes_count}
             <Link to={`/posts/${id}`}>
             {comments_count}
