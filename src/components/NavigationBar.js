ad28960d1397b226ef3bca21f81e4724334a7f97
jeremyhsimons <jeremysimons15@gmail.com>
2023-10-09 18:43:58 +0000

    update navbar with filter options.

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -40,6 +40,27 @@ const NavigationBar = () => {
         >
           Create a post
       </NavLink>
+      <NavLink 
+        className={styles.NavLink} 
+        activeClassName={styles.Active}
+        to="/bikes-and-cars" 
+        >
+          Bikes/Cars
+      </NavLink>
+      <NavLink 
+        className={styles.NavLink} 
+        activeClassName={styles.Active}
+        to="/electronics" 
+        >
+          Electronics
+      </NavLink>
+      <NavLink 
+        className={styles.NavLink} 
+        activeClassName={styles.Active}
+        to="/diy" 
+        >
+          DIY
+      </NavLink>
     </>
   )
   const loggedOutIcons = <><h1>Please Log in</h1></>
@@ -65,19 +86,7 @@ const NavigationBar = () => {
                             Sign In
                         </NavLink>
                         {currentUser ? loggedInIcons: loggedOutIcons}
-
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
                     </Nav>
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
