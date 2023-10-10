31451a751444deef8afbfbd24563ebf032a94497
jeremyhsimons <jeremysimons15@gmail.com>
2023-10-10 18:03:03 +0000

    Import and add infinite scroll component.

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -10,6 +10,7 @@ import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 
 import Post from "./Post";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function PostsPage({ message, filter="" }) {
   const [ posts, setPosts ] = useState({result: []});
@@ -58,9 +59,18 @@ function PostsPage({ message, filter="" }) {
         {hasLoaded ? (
           <>
             {posts.results.length ? (
-              posts.results.map(post => (
-                <Post key={post.id} {...post} setPosts={setPosts}/>
-              ))
+              <InfiniteScroll
+                children={
+                  posts.results.map(post => (
+                    <Post key={post.id} {...post} setPosts={setPosts}/>
+                  ))
+                }
+                dataLength={posts.results.length}
+                loader={<h3>Loading...</h3>}
+                hasMore={!!posts.next}
+                next={() => {}}
+              />
+              
             ) : (
               <Container>
                 <h2>{message}</h2>
