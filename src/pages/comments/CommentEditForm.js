6865c04d9be14d0dd1ea969eb81536568004a47f
jeremyhsimons <jeremysimons15@gmail.com>
2023-10-11 18:45:34 +0000

    add handleChange and handleSubmit functions.

--- src/pages/comments/CommentEditForm.js
+++ src/pages/comments/CommentEditForm.js
@@ -1,6 +1,39 @@
 import React from 'react'
 
-const CommentEditForm = () => {
+const CommentEditForm = (props) => {
+
+  const { id, content, setShowEditForm, setComments } = props;
+
+  const [formContent, setFormContent] = useState(content);
+
+  const handleChange = (event) => {
+    setFormContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put(`/comments/${id}/`, {
+        content: formContent.trim(),
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? {
+                ...comment,
+                content: formContent.trim(),
+                updated_at: "now",
+              }
+            : comment;
+        }),
+      }));
+      setShowEditForm(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="pr-1">
