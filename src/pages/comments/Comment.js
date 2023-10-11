53a5e4f9360160ec8467548f1f3ef27871f4d4d8
jeremyhsimons <jeremysimons15@gmail.com>
2023-10-11 18:09:58 +0000

    add destructured props to basic comment component.

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Media } from 'react-bootstrap'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
-const Comment = (props) => {
+const Comment = ({
+  profile_id, 
+    profile_image, 
+    author, 
+    updated_at, 
+    content,
+}) => {
   return (
-    <div>Comment</div>
+    <div>
+      <>
+        <hr />
+        <Media>
+          <Link to={`/profiles/${profile_id}`}>
+            <p>Image goes here</p>
+          </Link>
+          <Media.Body className="align-self-center ml-2">
+            <span className="">{author}</span>
+            <span className="">{updated_at}</span>
+            <p>{content}</p>
+          </Media.Body>
+        </Media>
+      </>
+    </div>
   )
 }
 
