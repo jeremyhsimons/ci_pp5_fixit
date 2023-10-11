c123ac6990726ab42a4b5ce6e8df2d07c2103418
jeremyhsimons <jeremysimons15@gmail.com>
2023-10-11 18:44:09 +0000

    Add boilerplate comment edit form

---
+++ src/pages/comments/CommentEditForm.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const CommentEditForm = () => {
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="pr-1">
+        <Form.Control
+          className={styles.Form}
+          as="textarea"
+          value={formContent}
+          onChange={handleChange}
+          rows={2}
+        />
+      </Form.Group>
+      <div className="text-right">
+        <button
+          className={styles.Button}
+          onClick={() => setShowEditForm(false)}
+          type="button"
+        >
+          cancel
+        </button>
+        <button
+          className={styles.Button}
+          disabled={!content.trim()}
+          type="submit"
+        >
+          save
+        </button>
+      </div>
+    </Form>
+  )
+}
+
+export default CommentEditForm
