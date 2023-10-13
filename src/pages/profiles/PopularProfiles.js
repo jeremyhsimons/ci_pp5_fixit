ffc23467f58826755631dd1ec6f318e5b43e4936
jeremyhsimons <jeremysimons15@gmail.com>
2023-10-13 14:14:22 +0000

    Create custom hook to provide profile data

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -3,31 +3,11 @@ import { Container } from 'react-bootstrap'
 import { axiosReq } from '../../api/axiosDefaults';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import Profile from './Profile';
+import { useProfileData } from '../../contexts/ProfileDataContext';
 
 const PopularProfiles = ({mobile}) => {
 
-  const [profileData, setProfileData] = useState({
-    pageProfile: { results: [] },
-    popularProfiles: { results: [] },
-  });
-  const {popularProfiles} = profileData;
-  const currentUser = useCurrentUser();
-
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const {data} = await axiosReq.get("/profiles/?ordering=stars_count")
-        setProfileData(prevState => ({
-          ...prevState,
-          popularProfiles: data,
-        }))
-      } catch (err) {
-        console.log(err)
-      }
-    }
-
-    handleMount();
-  }, [currentUser])
+  const { popularProfiles } = useProfileData();
 
   return (
     <Container className={`${mobile && "d-lg-none text-center mb-3"}`}>
