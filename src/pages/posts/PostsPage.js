e22b4a98a77b4fa383ecd08ffe42ea69a67ff855
jeremyhsimons <jeremysimons15@gmail.com>
2023-10-13 07:49:01 +0000

    Add boilerplate popular profiles list

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -12,6 +12,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function PostsPage({ message, filter="" }) {
   const [ posts, setPosts ] = useState({result: []});
@@ -43,7 +44,7 @@ function PostsPage({ message, filter="" }) {
   return (
     <Row className="h-100">
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <PopularProfiles />
       </Col>
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
---
+++ src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const PopularProfiles = () => {
+  return (
+    <Container>
+      <p>Trending contributors:</p>
+    </Container>
+  )
+}
+
+export default PopularProfiles
