9c2d6eaac46635d36dcc9bb01857a091bdba168d
jeremyhsimons <jeremysimons15@gmail.com>
2023-10-14 13:57:38 +0000

    Add image field to form

--- src/pages/profiles/ProfileEditForm.js
+++ src/pages/profiles/ProfileEditForm.js
@@ -19,16 +19,46 @@ const ProfileEditForm = () => {
             />
           </Form.Group>
           <Form.Group>
-          <Form.Label>Category</Form.Label>
-          <Form.Control as="select" name="category" value={category} onChange={handleChange}>
-            <option value="">Select...</option>
-            <option value="LFH">Looking for help</option>
-            <option value="SME">Subject matter expert</option>
-            <option value="JB">Just browsing</option>
-            <option value="AME">Ask me anything!</option>
-            <option value="NA">Not active</option>
-          </Form.Control>
-        </Form.Group>
+            <Form.Label>Category</Form.Label>
+            <Form.Control as="select" name="category" value={category} onChange={handleChange}>
+              <option value="">Select...</option>
+              <option value="LFH">Looking for help</option>
+              <option value="SME">Subject matter expert</option>
+              <option value="JB">Just browsing</option>
+              <option value="AME">Ask me anything!</option>
+              <option value="NA">Not active</option>
+            </Form.Control>
+          </Form.Group>
+        </Col>
+        <Col md={6}>
+          <Form.Group>
+            {image && (
+              <figure>
+                <Image src={image} fluid />
+              </figure>
+            )}
+            <div>
+                <Form.Label
+                  className={`btn my-auto`}
+                  htmlFor="image-upload"
+                >
+                  Change the image
+                </Form.Label>
+              </div>
+              <Form.File
+                id="image-upload"
+                ref={imageFile}
+                accept="image/*"
+                onChange={(e) => {
+                  if (e.target.files.length) {
+                    setProfileData({
+                      ...profileData,
+                      image: URL.createObjectURL(e.target.files[0]),
+                    });
+                  }
+                }}
+              />
+          </Form.Group>
         </Col>
       </Row>
     </Form>
