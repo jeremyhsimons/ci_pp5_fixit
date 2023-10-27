2bb1f4317694b6b23796520c14e9acd5a00fb294
jeremyhsimons <jeremysimons15@gmail.com>
2023-10-27 08:54:38 +0000

    Create logic for handling closing of message.

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Alert } from 'react-bootstrap'
 
 const Message = ({message}) => {
+
+  const [show, setShow] = useState(true);
+
+  const handeClose = (event) => {
+    event.preventDefault();
+    setShow(false);
+  };
+
   return (
-    <div>
-      <Alert variant='warning'>{message}</Alert>
+    <div >
+      <Alert variant='warning' onClick={handeClose}>{message}</Alert>
     </div>
   )
 }
